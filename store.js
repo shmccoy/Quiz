cb2002d7f16f8cdb6a5748da8adc86989d34ec4c
Sean M <53903852+shmccoy@users.noreply.github.com>
2019-11-05 17:40:14 -0500

    Edits

---
+++ store.js
@@ -0,0 +1,116 @@
+const STORE = {
+    questions: [ //1
+        {
+            question: "How many times has Robert Downey Jr. played Iron Man?",
+            options: [
+                "3",
+                "9",
+                "7",
+                "10"
+            ],
+            answer: "10"
+        },
+        //2
+        {
+            question: "Who is the brother to Wanda Maximoff aka Scarlet Witch?",
+            options: [
+                "Wolverine",
+                "Quicksilver",
+                "Doctor Strange",
+                "Hawkeye"
+            ],
+            answer: "Quicksilver"
+        },
+        //3
+        {
+            question: "Where is Wakanda located?",
+            options: [
+                "Australia",
+                "Europe",
+                "Africa",
+                "South America"
+            ],
+            answer: "Africa"
+        },
+        //4
+        {
+            question: "Who has made an appearance in almost every Marvel movie?",
+            options: [
+                "Stan Lee",
+                "Tony Stark",
+                "Jack Kirby",
+                "Thanos"
+            ],
+            answer: "Stan Lee"
+        },
+        //5
+        {
+            question: "What is the name of the main villain in the second Avengers film?",
+            options: [
+                "Thanos",
+                "Ultron",
+                "The Collector",
+                "Green Goblin"
+            ],
+            answer: "Ultron"
+        },
+        //6
+        {
+            question: "Who directed Thor (2011)?",
+            options: [
+                "Jon Favreau",
+                "Ron Howard",
+                "Kenneth Branagh",
+                "Steven Spielberg"
+            ],
+            answer: "Kenneth Branagh"
+        },
+        //7
+        {
+            question: 'In which film did Hawkey first appear?',
+            options: [
+                "The Avengers",
+                "The Incredible Hulk",
+                "Iron Man 2",
+                "Thor"
+            ],
+            answer: "Thor"
+        },
+        //8
+        {
+            question: "Who is Black Panther’s sister?",
+            options: [
+                "Ramonda",
+                "Shuri",
+                "Ororo",
+                "Nehanda"
+            ],
+            answer: "Shuri"
+        },
+        //9
+        {
+            question: "This person is second in command of S.H.I.E.L.D. in The Avengers.",
+            options: [
+                "Phil Coulson",
+                "Maria Hill",
+                "Nick Fury",
+                "Clint Barton"
+            ],
+            answer: "Maria Hill"
+        },
+        //10
+        {
+            question: "What part of Thor’s body does he lose in Thor: Ragnarok?",
+            options: [
+                "Eye",
+                "Arm",
+                "Leg",
+                "Foot"
+            ],
+            answer: "Eye"
+        },
+
+    ],
+    currentQuestion: 0,
+    score: 0
+};
